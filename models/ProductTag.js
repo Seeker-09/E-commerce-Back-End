e29055ff242e2896f4419da1618c33369bc3c03a
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-07-11 09:56:26 -0400

    established server connection using sequelize

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,7 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    product_tag_id: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
@@ -16,14 +16,14 @@ ProductTag.init(
       type: DataTypes.INTEGER,
       references: {
         model: "product",
-        id: "product_id"
+        id: "id"
       }
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "tag",
-        id: "tag_id"
+        id: "id"
       }
     }
   },
